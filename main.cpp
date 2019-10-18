e252a9092b72f69939e7690ff405e219674a4761
Steventime <32660539+Steventime0430@users.noreply.github.com>
2019-10-18 12:04:03 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,285 +1,251 @@
 
 #include <iostream>
-#include <string>
+#include <fstream>
 
+#include <string>
 using namespace std;
 
-class Base_Map {
-    
+
+// Use Map Class as the main funcional class
+class Map;
+
+// Use Block Class to insert the Block.
+class Block{
+    friend Map;
+
 private:
-    int row_matrix,col_matrix; // the size of the map to be outputed
-    int start_position;   //the starting position of the block
-    int type;  // the block type
-    int **map; // 2D array matrix
-    int block_matrix[4][4]; // store the block
-    int T[19][4][4]={
-        {0, 1, 0, 0,
-            1, 1, 1, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, // T[0]=T1
-        
-        {1, 1, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            0, 0, 0, 0},// T[1]=L1
-        
-        {1, 1, 0, 0,
-            0, 1, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 0, 0}, //T[2]=J1
-        
-        {1, 1, 0, 0,
-            0, 1, 1, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[3]=S1
-        
-        {1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0}, //T[4]=I1
-        
-        {0, 1, 0, 0,
-            1, 1, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 0, 0}, //T[5]=T2
-        
-        {1, 0, 0, 0,
-            1, 1, 1, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[6]=L2
-        
-        {1, 1, 1, 0,
-            1, 0, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[7]=J2
-        
-        {0, 1, 0, 0,
-            1, 1, 0, 0,
-            1, 0, 0, 0,
-            0, 0, 0, 0}, //T[8]=S2
-        
-        {1, 1, 1, 1,
-            0, 0, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[9]=I2
-        
-        {1, 1, 1, 0,
-            0, 1, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[10]=T3
-        
-        {0, 1, 0, 0,
-            0, 1, 0, 0,
-            1, 1, 0, 0,
-            0, 0, 0, 0}, //T[11]=L3
-        
-        {1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 1, 0, 0,
-            0, 0, 0, 0}, //T[12]=J3
-        
-        {0, 1, 1, 0,
-            1, 1, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[13]=Z1
-        
-        {1, 1, 0, 0,
-            1, 1, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[14]=O
-        
-        {1, 0, 0, 0,
-            1, 1, 0, 0,
-            1, 0, 0, 0,
-            0, 0, 0, 0}, //T[15]=T4
-        
-        {1, 1, 1, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[16]=L4
-        
-        {0, 0, 1, 0,
-            1, 1, 1, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0}, //T[17]=J4
-        
-        {1, 0, 0, 0,
-            1, 1, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 0, 0} //T[18]=Z2
-    };
+    int Block_Row_Num,Block_Col_Num;
+    // Use Array Pointer to represent the Block.
+    int *Block_Array;
+    int Type;
+    int T1[6]={0,1,0,1,1,1}; //Row*Col=2*3
+    int L1[6]={1,1,1,0,1,0}; //3*2
+    int J1[6]={1,1,0,1,0,1}; //3*2
+    int S1[6]={1,1,0,0,1,1}; //2*3
+    int I1[4]={1,1,1,1};     //4*1
+    int T2[6]={0,1,1,1,0,1}; //3*2
+    int L2[6]={1,0,0,1,1,1}; //2*3
+    int J2[6]={1,1,1,1,0,0}; //2*3
+    int S2[6]={0,1,1,1,1,0}; //3*2
+    int I2[4]={1,1,1,1};     //1*4
+    int T3[6]={1,1,1,0,1,0}; //2*3
+    int L3[6]={0,1,0,1,1,1}; //3*2
+    int J3[6]={1,0,1,0,1,1}; //3*2
+    int Z1[6]={0,1,1,1,1,0}; //2*3
+    int O[4]={1,1,1,1};      //2*2
+    int T4[6]={1,0,1,1,1,0}; //3*2
+    int L4[6]={1,1,1,0,0,1}; //2*3
+    int J4[6]={0,0,1,1,1,1}; //2*3
+    int Z2[6]={1,0,1,1,0,1}; //3*2
     
 public:
     
-    // constructor | 建構子 | Build the Base map matrix
-    Base_Map ():row_matrix(0),col_matrix(0){}
+    // Constructor
+    Block():Block_Row_Num(0),Block_Col_Num(0),Block_Array(nullptr){}
     
-    Base_Map (int Rows,int Columns):row_matrix(Rows),col_matrix(Columns){
-        // Creating Map matrix with (Rows+5)*(Columns+3)
-        // 在 Map 中 Row=0 的元素設為 1，供後續方塊掉落來判斷是否撞到。
-        // 在 Map 中 存放輸出矩陣上多存四列，供方塊掉落前暫存，並提供後續遊戲結束的判斷。（Condition 2）
-        map = new int*[row_matrix+5];
-        for (int i=0;i<row_matrix+5;i++){
-            
-            // 將 Columns 多加三行，主要供方塊掉落時為 0 的格子能有相對應的量值。
-            // 但由於皆無非法測資，故此三行的所有量值除了 Rows=0 應皆為 0
-            map[i]=new int[col_matrix+3];
-            for(int j=0;j<col_matrix+3;j++){
-                map[i][j]=0;
-            }
-        }
-        for(int k=0;k<col_matrix+3;k++){
-            map[0][k]=1;
-        }
-    }
-    // destructor | 解構子
-    ~Base_Map(){
-        for (int i = 0; i < row_matrix+5; i++){
-            delete[]map[i]; // Delete Columns
-        }
-        delete[]map; // Delete Rows
+    Block(int Row_Num,int Col_Num,int *Block_type):
+    Block_Row_Num(Row_Num),Block_Col_Num(Col_Num){
+        Block_Array=Block_type;
     }
     
-    
-    // Print every element in map in order | 由上而下依序印出
-    void Game_over() {
-        for(int i= row_matrix;i>0;i--){
-            for (int j=0;j<col_matrix;j++){
-                cout<< map[i][j];
-            }
-            cout<<endl;
-        }
-    }
-    
-    // Load the input block and transform it from string to int
-    // Input the block_matrix and re-arranging the map.
-    void InsertBlock(string Block_Type,int start){
-        
-        start_position=start;
-        if (Block_Type=="T1") type=0;
-        else if (Block_Type=="L1")type= 1;
-        else if (Block_Type=="J1")type= 2;
-        else if (Block_Type=="S1")type= 3;
-        else if (Block_Type=="I1")type= 4;
-        else if (Block_Type=="T2")type= 5;
-        else if (Block_Type=="L2")type= 6;
-        else if (Block_Type=="J2")type= 7;
-        else if (Block_Type=="S2")type= 8;
-        else if (Block_Type=="I2")type= 9;
-        else if (Block_Type=="T3")type= 10;
-        else if (Block_Type=="L3")type= 11;
-        else if (Block_Type=="J3")type= 12;
-        else if (Block_Type=="Z1")type= 13;
-        else if (Block_Type=="O") type= 14;
-        else if (Block_Type=="T4")type= 15;
-        else if (Block_Type=="L4")type= 16;
-        else if (Block_Type=="J4")type= 17;
-        else if (Block_Type=="Z2")type= 18;
-        Change_Block_Matrix(T[type]);
-        this->Rearranging();
-    }
-    // Transform from 3D matrix to 2D matrix
-    void Change_Block_Matrix (int block[4][4]){
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                block_matrix[i][j]=block[i][j];
-            }
-        }
-    }
-    
-    // Re-Arranging the map
-    void Rearranging (){
-        // current_row 是 block 最底層的位置，從row_matrix開始往下掉，但其16格觸底前，先印到 map 上
-        for (int current_row=row_matrix+1;current_row>0;current_row--){
-            if (hit_the_block(current_row)==true){
-                for( int s=current_row;s<current_row+4;s++){
-                    for(int j=start_position-1;j<start_position+3;j++){
-                        map[s][j]+= block_matrix[s-current_row][j-start_position+1];
-                    }
-                }
-                Row_filled(current_row); //判斷是否填滿，並進行平移
-                break;
-            }
-        }
-    }
-    
-    // 判斷16格是否撞到方格
-    bool hit_the_block(int cur_row) {
-        
-        for(int i=cur_row;i<cur_row+4;i++){
-            for (int j=start_position-1;j<start_position+3;j++){
-                // 當下一 row 的 map element 和 block 總和為二，表示不能再降落了。
-                if(map[i-1][j]+block_matrix[i-cur_row][j-start_position+1]==2) return true;
-            }
-        }
-        return false;
-    }
-    
-    //判斷同一列是否已經達到可消除的狀態
-    void Row_filled (int current_row){
-        
-        for(int i=current_row+3;i>=current_row;i--){    // current=1, i=4
-            int sum=0;
-            for(int j=0;j<col_matrix;j++){      // 0~col_matrix
-                sum+=map[i][j];
-                if(sum==col_matrix){
-                    //copy the elements
-                    for(int r=i;r<row_matrix+4;r++){
-                        for(int k=0;k<col_matrix;k++){
-                            map[r][k]=map[r+1][k];
-                        }
-                    }
-                    
-                }
-            }
-        }
-    }
-    // To determing whether there are still blocks left in the temperate storages.
-    // If returning TRUE, Game is over.
-    bool temp_storage_check(){
-        for(int i=row_matrix+1;i<row_matrix+5;i++){
-            for (int j=0;j<col_matrix;j++){
-                if(map[i][j]==1)return true;
-            }
-        }
-        return false;
+    // To return the Block with Rows, Columns, and the Block_Array
+    Block Insert_Block(string Block_Type){
+        if (Block_Type=="T1")  return Block(2,3,T1);
+        else if (Block_Type=="L1") return Block(3,2,L1);
+        else if (Block_Type=="J1") return Block(3,2,J1);
+        else if (Block_Type=="S1") return Block(2,3,S1);
+        else if (Block_Type=="I1") return Block(4,1,I1);
+        else if (Block_Type=="T2") return Block(3,2,T2);
+        else if (Block_Type=="L2") return Block(2,3,L2);
+        else if (Block_Type=="J2") return Block(2,3,J2);
+        else if (Block_Type=="S2") return Block(3,2,S2);
+        else if (Block_Type=="I2") return Block(1,4,I2);
+        else if (Block_Type=="T3") return Block(2,3,T3);
+        else if (Block_Type=="L3") return Block(3,2,L3);
+        else if (Block_Type=="J3") return Block(3,2,J3);
+        else if (Block_Type=="Z1") return Block(2,3,Z1);
+        else if (Block_Type=="O")  return Block(2,2,O);
+        else if (Block_Type=="T4") return Block(3,2,T4);
+        else if (Block_Type=="L4") return Block(2,3,L4);
+        else if (Block_Type=="J4") return Block(2,3,J4);
+        else if (Block_Type=="Z2") return Block(3,2,Z2);
+        else return Block();
     }
 };
 
 
+class Map{
+
+private:
+    int Map_Row_Num,Map_Col_Num;
+    int Block_Start_Position;
+    int *Map_Array;
+    int *Temp_Array;
+    Block S;
+    
+public:
+    // constructor | 建構子 | Build the Map base in Array Pointer
+    Map():Map_Row_Num(0),Map_Col_Num(0){}
+    
+    Map(int Rows,int Columns):Map_Row_Num(Rows),Map_Col_Num(Columns){
+        // Creating Map matrix with (Rows+5)*(Columns)
+        // 在 Map 中 Row=0 的元素設為 1，供後續方塊掉落來判斷是否撞到。
+        // 在 Map 中 存放輸出矩陣上多存四列，供方塊掉落前暫存，並提供後續遊戲結束的判斷。（Condition 2）
+        int Size= (Map_Row_Num+5)*Map_Col_Num;
+        Map_Array = new int[Size]{0};
+        for (int i=0;i<Map_Col_Num;i++){
+            Map_Array[i]=1;
+        }
+    }
+    
+    ~Map(){
+        delete []Map_Array;
+        delete []Temp_Array;
+    }
+    // To print the map from the top
+    void Print_Map();
+    
+    // Used to input the Block type and Re-Arrange the map array.
+    void Re_Arrange(string Block_Type,int S);
+    
+    // To determine where the Block would stop
+    bool Hit_Wall(int Current_Row);
+    
+    // To check whether the row is full.
+    // If so, move the elements.
+    void Row_Filled(int Current_Row);
+    
+    // To check whether there is still elements in the temperate storages after shifting.
+    bool Storage_Left();
+};
 
 
+void Map::Print_Map(){
+    
+    for (int i=Map_Row_Num;i>0;i--){
+        for (int j=0;j<Map_Col_Num;j++){
+            cout << Map_Array[j+i*Map_Col_Num];
+        }
+        cout<<endl;
+    }
+}
+
+void Map::Re_Arrange(string Block_Type,int Start_Position){
+
+    // Input the Starting Position and the Block as S
+    Block_Start_Position=Start_Position;
+    S=S.Insert_Block(Block_Type);
+    int Current_Row;
+    int Current_Col=Block_Start_Position-1;
+    
+    // Current 從 Map 上方一格開始 Ex：Row_Number=2, Current=3。
+    for(Current_Row=Map_Row_Num+1;Current_Row>0;Current_Row--){
+        
+        // If not hiting the row, move one current row down.
+        if(Hit_Wall(Current_Row)==true){
+            // If so, change the element to 1 in the map.
+            for(int i=Current_Row;i<Current_Row+S.Block_Row_Num;i++){
+                for(int j=Current_Col;j<Current_Col+S.Block_Col_Num;j++){            Map_Array[i*Map_Col_Num+j]+=S.Block_Array[j-Current_Col+(i-Current_Row)*S.Block_Col_Num];
+                }
+            }
+            // Check whether it is full or not.
+            Row_Filled(Current_Row);
+            break;
+        }
+    }
+}
+
+bool Map::Storage_Left(){
+
+    for (int i=Map_Row_Num+4;i>Map_Row_Num;i--){
+        for (int j=0;j<Map_Col_Num;j++){
+            if(Map_Array[j+i*Map_Col_Num]!=0){
+                return true;
+            };
+        }
+    }
+    return false;
+}
+
+bool Map::Hit_Wall(int Current_Row){
+    
+    // Assign a temperate array pointer to determine hitting the wall.
+    int Current_Col=Block_Start_Position-1;
+    Temp_Array=new int[S.Block_Col_Num*S.Block_Row_Num];
+    for(int i=0;i<S.Block_Col_Num*S.Block_Row_Num;i++){
+        Temp_Array[i]=0;
+    }
+    
+    // Temperate Array 會紀錄所有Block 遇到落下那一格加起來的量值。
+    for(int i=Current_Row;i<Current_Row+S.Block_Row_Num;i++){
+        for(int j=Current_Col;j<Current_Col+S.Block_Col_Num;j++){
+            Temp_Array[j-Current_Col+(i-Current_Row)*S.Block_Col_Num]=
+                Map_Array[(i-1)*Map_Col_Num+j]+
+                S.Block_Array[j-Current_Col+(i-Current_Row)*S.Block_Col_Num];
+            }
+    }
+    // 若 Temperate Array 量值為2，則表示撞到牆，即方塊或最下面一列。（ Block ==1 && Map ==1 )
+    for (int i=0;i<S.Block_Row_Num*S.Block_Col_Num;i++){
+        if(Temp_Array[i]==2)return true;
+    }
+    return false;
+}
 
 
+void Map::Row_Filled(int Current_Row){
+    
+    // 從上而下確認是否 Row 的所有元素是滿的。
+    for ( int i=Current_Row+3;i>=Current_Row;i--){
+        int Count=0;
+        for (int j=0;j<Map_Col_Num;j++){
+            Count+=Map_Array[i*Map_Col_Num+j];
+        }
+        
+        // 當 Row 滿時，將所有 Row 以上的格子往下移一格。
+        if(Count==Map_Col_Num){
+            
+            for(int k=i;k<=Map_Row_Num+3;k++){
+                for(int r=0;r<Map_Col_Num;r++){
+                    Map_Array[k*Map_Col_Num+r]=Map_Array[(k+1)*Map_Col_Num+r];
+                }
+            }
+            for(int r=0;r<Map_Col_Num;r++){
+                Map_Array[(Map_Row_Num+4)*Map_Col_Num+r]=0;
+            }
+        }
+    }
+
+}
 
 
-
-
-int main() {
+int main(){
     
     int Rows,Columns,Start_Position;
     string Block_Type;
     
-    // Creating the Base_Map matrix
+    // ofstream fout;
+    
+    
+    // Creating the Tetris
     cin >> Rows >> Columns;
-    Base_Map Tetris(Rows,Columns);
+    Map Tetris(Rows,Columns);
     
     // Input the block and starting position until
     // (1): "End" to print the map.
-    // (2): There are still blocks in temperate storages uppon the map after erasing.
+    // (2): There are still blocks in temperate storages uppon the map after shifting.
     
     while (cin >> Block_Type){
         if(Block_Type=="End"){
             // Condition 1
-            Tetris.Game_over();
+            Tetris.Print_Map();
             break;
         }
         else{
             // Condition 2
             cin >> Start_Position;
-            Tetris.InsertBlock(Block_Type,Start_Position);
-            if(Tetris.temp_storage_check()){
-                Tetris.Game_over();
+            Tetris.Re_Arrange(Block_Type,Start_Position);
+            if(Tetris.Storage_Left()==true){
+                Tetris.Print_Map();
                 break;
             }
         }
